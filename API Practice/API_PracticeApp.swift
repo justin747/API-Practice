a9898b424b4d29e616a5f421de5c652b9441c009
justin747 <justin747@gmail.com>
2023-05-03 14:06:51 -0700

    Initial Commit

---
+++ API Practice/API_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PracticeApp.swift
+//  API Practice
+//
+//  Created by Justin on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct API_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
